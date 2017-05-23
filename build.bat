6954106f0f16fd91bee66f9f084175d8f55c0acb
Wohlstand <admin@wohlnet.ru>
2017-05-23 18:08:26 +0300

    Add support for AppVeyor CI for Win32 auto builds

---
+++ build.bat
@@ -0,0 +1,28 @@
+@echo off
+call _paths.bat
+
+PATH=%QtDir%;%MinGW%;%GitDir%;%PATH%
+SET SEVENZIP=C:\Program Files\7-Zip
+
+IF NOT EXIST "%SEVENZIP%\7z.exe" SET SEVENZIP=%ProgramFiles(x86)%\7-Zip
+IF NOT EXIST "%SEVENZIP%\7z.exe" SET SEVENZIP=%ProgramFiles%\7-Zip
+
+qmake FMBankEdit.pro CONFIG+=release CONFIG-=debug
+IF ERRORLEVEL 1 goto error
+
+mingw32-make
+IF ERRORLEVEL 1 goto error
+
+md opl3-bank-editor
+cd bin-release
+windeployqt opl3_bank_editor.exe
+IF ERRORLEVEL 1 goto error
+cd ..
+
+"%SEVENZIP%\7z" a -tzip "opl3-bank-editor\opl3-bank-editor-dev-win32.zip" bin-release\
+
+goto quit
+:error
+echo ==============BUILD ERRORED!===============
+:quit
+
