176d0659469a65d8da638a755726f6264aa74542
JP Cimalando <jp-dev@inbox.ru>
2018-05-03 18:31:34 +0200

    second array bounds fix

--- src/opl/generator.cpp
+++ src/opl/generator.cpp
@@ -673,7 +673,7 @@ void Generator::NoteOffAllChans()
     }
     else
     {
-        for(uint32_t c = 0; c < NUM_OF_CHANNELS; ++c)
+        for(uint32_t c = 0; c < USED_CHANNELS_2OP; ++c)
             NoteOff(channels[c]);
     }
 }
