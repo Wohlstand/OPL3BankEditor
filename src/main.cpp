9c3c84b0a53e875653de2db5dbe948761ae2025f
JP Cimalando <jp-dev@inbox.ru>
2018-05-20 05:30:14 +0200

    add dynamic language choice

--- src/main.cpp
+++ src/main.cpp
@@ -16,37 +16,16 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "main.h"
 #include "bank_editor.h"
-#include <QApplication>
-#include <QTranslator>
 #include <QLibraryInfo>
 #include <QStringList>
 #include <QDebug>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-
-#if defined(Q_OS_WIN)
-    QString qtTranslationDir = QCoreApplication::applicationDirPath() + "/translations";
-    const QString &appTranslationDir = qtTranslationDir;
-#elif defined(Q_OS_DARWIN)
-    QString qtTranslationDir = QCoreApplication::applicationDirPath() + "/../Resources/translations";
-    const QString &appTranslationDir = qtTranslationDir;
-#else
-    QString qtTranslationDir = QLibraryInfo::location(QLibraryInfo::TranslationsPath);
-    QString appTranslationDir =
-        QCoreApplication::applicationDirPath() + "/../share/opl3_bank_editor/translations";
-#endif
-
-    // install a translator of Qt messages
-    QTranslator qtTranslator;
-    qtTranslator.load("qt_" + QLocale::system().name(), qtTranslationDir);
-    a.installTranslator(&qtTranslator);
-    // install a translator of application messages
-    QTranslator myappTranslator;
-    myappTranslator.load(QLocale::system().name(), appTranslationDir);
-    a.installTranslator(&myappTranslator);
+    Application a(argc, argv);
+    a.translate(QLocale::system().name());
 
     BankEditor w;
     w.show();
@@ -56,4 +35,41 @@ int main(int argc, char *argv[])
         w.openFile(args[1]);
 
     return a.exec();
+}
+
+Application::Application(int &argc, char **argv)
+    : QApplication(argc, argv)
+{
+    installTranslator(&m_qtTranslator);
+    installTranslator(&m_appTranslator);
+}
+
+void Application::translate(const QString &language)
+{
+    if (language.isEmpty())
+        return translate(QLocale::system().name());
+    m_qtTranslator.load("qt_" + language, getQtTranslationDir());
+    m_appTranslator.load("opl3bankeditor_" + language, getAppTranslationDir());
+    emit languageChanged();
+}
+
+QString Application::getQtTranslationDir() const
+{
+#if defined(Q_OS_WIN)
+    return QCoreApplication::applicationDirPath() + "/translations";
+#elif defined(Q_OS_DARWIN)
+    return QCoreApplication::applicationDirPath() + "/../Resources/translations";
+#else
+    return QLibraryInfo::location(QLibraryInfo::TranslationsPath);
+#endif
+}
+
+QString Application::getAppTranslationDir() const
+{
+#if defined(Q_OS_WIN) || defined(Q_OS_DARWIN)
+    return getQtTranslationDir();
+#else
+    QString qtTranslationDir = QLibraryInfo::location(QLibraryInfo::TranslationsPath);
+    return QCoreApplication::applicationDirPath() + "/../share/opl3_bank_editor/translations";
+#endif
 }
---
+++ src/main.h
@@ -0,0 +1,44 @@
+/*
+ * OPL Bank Editor by Wohlstand, a free tool for music bank editing
+ * Copyright (c) 2018 Vitaly Novichkov <admin@wohlnet.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QApplication>
+#include <QTranslator>
+
+class Application : public QApplication
+{
+    Q_OBJECT
+
+public:
+    Application(int &argc, char **argv);
+
+    static Application *instance()
+        { return static_cast<Application *>(qApp); }
+
+    QString getQtTranslationDir() const;
+    QString getAppTranslationDir() const;
+
+public slots:
+    void translate(const QString &language);
+
+signals:
+    void languageChanged();
+
+private:
+    QTranslator m_qtTranslator;
+    QTranslator m_appTranslator;
+};
