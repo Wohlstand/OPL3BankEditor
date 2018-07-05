063380198dba5a2ab5846ba1410af5f36301d6a3
JP Cimalando <jp-dev@inbox.ru>
2018-07-05 17:03:16 +0200

    add @Wohlstand's proxy style to fix QComboBox width

---
+++ src/proxystyle.h
@@ -0,0 +1,39 @@
+/*
+ * OPL Bank Editor by Wohlstand, a free tool for music bank editing
+ * Copyright (c) 2018 Vitaly Novichkov <admin@wohlnet.ru>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#ifndef PROXYSTYLE_H
+#define PROXYSTYLE_H
+
+#if !defined(IS_QT_4)
+
+#include <QProxyStyle>
+
+class QWidget;
+class BankEditor_ProxyStyle : public QProxyStyle
+{
+public:
+    explicit BankEditor_ProxyStyle(QStyle *style = Q_NULLPTR);
+    explicit BankEditor_ProxyStyle(const QString &key);
+
+    int styleHint(StyleHint hint, const QStyleOption *option, const QWidget *widget, QStyleHintReturn *returnData) const;
+};
+
+#endif  // !defined(IS_QT_4)
+
+#endif // PROXYSTYLE_H
