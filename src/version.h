fb338221d2bb6a856288b3162a9ba44a3dcb955d
Wohlstand <admin@wohlnet.ru>
2025-01-01 03:26:42 +0300

    Forgot to update year here too

--- src/version.h
+++ src/version.h
@@ -33,6 +33,6 @@
 #define COPYRIGHT_SIGN "©"
 #endif
 
-#define COPYRIGHT COPYRIGHT_SIGN " 2016-2024, Vitaly Novichkov \"Wohlstand\""
+#define COPYRIGHT COPYRIGHT_SIGN " 2016-2025, Vitaly Novichkov \"Wohlstand\""
 
 #endif // VERSION_H
